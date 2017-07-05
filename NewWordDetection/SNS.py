e341d226f769ac4e36b762437aff1e6203ee7238
snowmanPeter <zjutedu1001@163.com>
2017-07-05 13:17:16 +0800

    互联网时代的社会语言学：基于SNS的文本数据挖掘 http://www.matrix67.com/blog/archives/5044

---
+++ NewWordDetection/SNS.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+
+
+class SNS():
+
+    def __init__(self,filename,max_len=4,fixed=1000,freedom=3):
+        self.filename = filename
+        self.max_len = max_len
+        self.fixed = fixed
+        self.freedom = freedom
+
+
+    def getStat(self):
+
+
