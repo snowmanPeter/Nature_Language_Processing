f9aa94057f113095b28b08ca76bfe08a69a951a3
master <zjutedu1001@163.com>
2017-07-05 21:23:14 +0800

    修改版本的新词发现算法 SNS

--- NewWordDetection/SNS.py
+++ NewWordDetection/SNS.py
@@ -6,11 +6,10 @@ import re
 
 class SNS():
 
-    cut_dict = ["\n","\t","\r","，","。","《","》","（","）","“","”","：","！","？","…","、"]
-    wordLen = {2:"(..)",3:"(...)",4:"(....)",5:"(.....)"}
-    rem = {1:"(.)%s",2:"{0}%s"}
+    cut_dict = ["\n","\t","\r","，","。","《","》","（","）","“","”","：","！","？","…","、","——"]
+    wordLen = {2:"(..)",3:"(...)",4:"(....)",5:"(.....)",6:"(......)",7:"(.......)",8:"(........)",9:"(.........)",10:"(..........)"}
 
-    def __init__(self,filename,max_len=5,fixed=2000,freedom=6):
+    def __init__(self,filename,max_len=5,fixed=500,freedom=3):
         self.filename = filename
         self.max_len = max_len
         self.fixed = fixed
@@ -19,6 +18,7 @@ class SNS():
 
     def getStat(self):
         result = []
+        fidd = open("result.txt","w")
         with open(self.filename,"r") as fid:
             self.text = "".join(fid.readlines())
             for i in self.cut_dict:
@@ -26,30 +26,37 @@ class SNS():
             self.text = self.text.decode("utf-8")
         print "统计词频长度为",len(self.text)
         word = pd.Series(list(self.text)).value_counts() / len(self.text)
-        print len(word)
+        for w in word.index:
+            fidd.write(w.encode("utf-8") + "\t" + str(word[w]) + "\n")
         for i in xrange(2,self.max_len+1):
             arr = []
             for k in xrange(i):
                 arr = arr + re.findall(self.wordLen[i],self.text[k:])
             rst = pd.Series(arr).value_counts()
-            rst = rst[rst>10]
-            word = word.append(rst / len(arr))
+            rst = rst[rst>5] / len(arr)
+            for r in rst.index:
+                fidd.write(r.encode("utf-8")+"\t"+str(rst[r])+"\n")
+            word = word.append(rst)
             for kk in xrange(i-1):
                 result = result + list(rst[np.array(list(map(lambda ss:rst[ss]/word[ss[kk]]/word[ss[kk+1:]],rst.index))) >= self.fixed].index)
         print "词频统计结束,"
+        fidd.close()
         return result
 
+    def entropy(self,result):
+        return -np.sum(result * np.log2(result))
+
     def getResult(self,rst):
+        print u'战战兢兢' in rst
         for w in rst:
-            entropy = []
-            for i in range(1,3):
-                temp = re.findall("(.)%s"%w,self.text)
-                jiegu = np.array(pd.Series(temp).value_counts()/len(temp))
-                entropy.append(-np.sum(jiegu*np.log2(jiegu)))
-            if min(entropy) >= self.freedom:
-                print w
-
-
+            temp = re.findall("(.)%s(.)"%w,self.text)
+            dw = pd.DataFrame(temp,columns=['left','right'])
+            left = dw['left'].value_counts()/len(dw['left'])
+            right = dw['right'].value_counts()/len(dw['right'])
+            if u'战战兢兢' == w:
+                self.entropy(left), self.entropy(right)
+            # if min(self.entropy(left),self.entropy(right)) > self.freedom:
+            #     print w,self.entropy(left),self.entropy(right)
 
 if __name__ == '__main__':
     filename = "Journey to the West.txt"
